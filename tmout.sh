7dc2e07380a2cb4bf225451abd93447b3112c2db
CSwede <christoffer.schols@gmail.com>
2016-10-10 12:40:28 -0400

    Create tmout.sh

---
+++ tmout.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# $TMOUT
+# If the $TMOUT environmental variable is set to a non-zero value time, then the shell prompt will time out after $time seconds. 
+# This will cause a logout.
+
+TMOUT=3         # Prompt times out at three seconds.
+
+echo "What is your favorite song?"
+echo "Quickly now, you only have $TMOUT seconds to answer!"
+read song
+
+if [ -z "$song" ]
+then
+  song="(no answer)"
+  # Default response.
+fi
+
+echo "Your favorite song is $song."
